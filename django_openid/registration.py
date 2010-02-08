98ae4233460b1ba9845a18874c528efaf81202e6
Bela Hausmannn <post@belahausmann.name>
2010-02-08 18:59:06 +0100

    ugettext_lazy() in combination with get_or_create() failed with DatabaseError "can't adapt" - fixed it by using ugettext()

--- django_openid/registration.py
+++ django_openid/registration.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 from django.conf import settings
@@ -19,68 +20,70 @@ class RegistrationConsumer(AuthConsumer):
        'Would you like to create an account?')
     c_already_confirmed_message = \
         ugettext_lazy('Your account is already confirmed')
-    
+
     register_template = 'django_openid/register.html'
     set_password_template = 'django_openid/set_password.html'
     confirm_email_template = 'django_openid/register_confirm_email.txt'
     register_email_sent_template = 'django_openid/register_email_sent.html'
     register_complete_template = 'django_openid/register_complete.html'
-    
+
     after_registration_url = None # None means "show a message instead"
-    unconfirmed_group = ugettext_lazy('Unconfirmed users')
-    
+
+    # ugettext_lazy cannot be used or mark_user_unconfirmed() will fail
+    unconfirmed_group = _('Unconfirmed users')
+
     # Registration options
     reserved_usernames = ['security', 'info', 'admin']
-    no_duplicate_emails = True    
+    no_duplicate_emails = True
     confirm_email_addresses = True
-    
+
     confirm_email_from = None # If None, uses settings.DEFAULT_FROM_EMAIL
     confirm_email_subject = ugettext_lazy('Confirm your e-mail address')
     confirm_link_secret = None
     confirm_link_salt = 'confirm-link-salt'
-    
+
     # sreg
     sreg = ['nickname', 'email', 'fullname']
-    
+
     RegistrationForm = forms.RegistrationFormPasswordConfirm
     ChangePasswordForm = forms.ChangePasswordForm
-    
+
     def user_is_confirmed(self, user):
         return not self.user_is_unconfirmed(user)
-    
+
     def user_is_unconfirmed(self, user):
         return user.groups.filter(name = self.unconfirmed_group).count()
-    
+
     def mark_user_unconfirmed(self, user):
         from django.contrib.auth.models import Group
         user.is_active = False
         user.save()
-        group, _ = Group.objects.get_or_create(name = self.unconfirmed_group)
+        group, created = Group.objects.get_or_create(name = self.unconfirmed_group)
         user.groups.add(group)
-    
+
     def mark_user_confirmed(self, user):
         user.groups.filter(name = self.unconfirmed_group).delete()
-    
+
     def get_registration_form_class(self, request):
         return self.RegistrationForm
-    
+
     def get_change_password_form_class(self, request):
         return self.ChangePasswordForm
-    
+
     def show_i_have_logged_you_in(self, request):
         return self.show_message(
             request, _('You are logged in'),
             _('You already have an account for that OpenID. ' \
             'You are now logged in.')
         )
-    
+
     def do_register_complete(self, request):
-        
+
         def on_success(request, identity_url, openid_response):
             # We need to behave differently from the default AuthConsumer
             # success behaviour. For simplicity, we do the following:
             # 1. "Log them in" as that OpenID i.e. stash it in the session
-            # 2. If it's already associated with an account, log them in as 
+            # 2. If it's already associated with an account, log them in as
             #    that account and show a message.
             # 2. If NOT already associated, redirect back to /register/ again
             openid_object = OpenID.from_openid_response(openid_response)
@@ -95,35 +98,35 @@ class RegistrationConsumer(AuthConsumer):
                 ))
             self.persist_openid(request, response, openid_object)
             return response
-        
+
         return self.dispatch_openid_complete(request, {
             consumer.SUCCESS: on_success,
-            consumer.CANCEL: 
-                lambda request, openid_response: self.do_register(request, 
+            consumer.CANCEL:
+                lambda request, openid_response: self.do_register(request,
                     message = self.request_cancelled_message
                 ),
-            consumer.FAILURE: 
-                lambda request, openid_response: self.do_register(request, 
+            consumer.FAILURE:
+                lambda request, openid_response: self.do_register(request,
                     message = self.failure_message % openid_response.message
                 ),
-            consumer.SETUP_NEEDED: 
-                lambda request, openid_response: self.do_register(request, 
+            consumer.SETUP_NEEDED:
+                lambda request, openid_response: self.do_register(request,
                     message = self.setup_needed_message
                 ),
         })
-    
+
     def on_registration_complete(self, request):
         if self.after_registration_url:
             return HttpResponseRedirect(self.after_registration_url)
         else:
             return self.render(request, self.register_complete_template)
-    
+
     def do_register(self, request, message=None):
-        # Show a registration / signup form, provided the user is not 
+        # Show a registration / signup form, provided the user is not
         # already logged in
         if not request.user.is_anonymous():
             return self.show_already_signed_in(request)
-        
+
         # Spot incoming openid_url authentication requests
         if request.POST.get('openid_url', None):
             return self.start_openid_process(request,
@@ -133,16 +136,16 @@ class RegistrationConsumer(AuthConsumer):
                 ),
                 trust_root = self.trust_root or urlparse.urljoin(request.path, '..')
             )
-        
+
         RegistrationForm = self.get_registration_form_class(request)
-        
+
         try:
             openid = request.openid and request.openid.openid or None
         except AttributeError:
             return self.show_error(
                 request, 'Add CookieConsumer or similar to your middleware'
             )
-        
+
         if request.method == 'POST':
             # TODO: The user might have entered an OpenID as a starting point,
             # or they might have decided to sign up normally
@@ -168,7 +171,7 @@ class RegistrationConsumer(AuthConsumer):
                 reserved_usernames = self.reserved_usernames,
                 no_duplicate_emails = self.no_duplicate_emails
             )
-        
+
         return self.render(request, self.register_template, {
             'form': form,
             'message': message,
@@ -179,19 +182,19 @@ class RegistrationConsumer(AuthConsumer):
             'no_thanks': self.sign_next(request.path),
             'action': request.path,
         })
-    
+
     def confirm_email_step(self, request, user):
         self.mark_user_unconfirmed(user)
         self.send_confirm_email(request, user)
         return self.render(request, self.register_email_sent_template, {
             'email': user.email,
         })
-    
+
     def generate_confirm_code(self, user):
         return signed.sign(int_to_hex(user.id), key = (
             self.confirm_link_secret or settings.SECRET_KEY
         ) + self.confirm_link_salt)
-    
+
     def send_confirm_email(self, request, user):
         from_email = self.confirm_email_from or settings.DEFAULT_FROM_EMAIL
         code = self.generate_confirm_code(user)
@@ -207,7 +210,7 @@ class RegistrationConsumer(AuthConsumer):
             from_email = from_email,
             recipient_list = [user.email]
         )
-    
+
     def do_c(self, request, token = ''):
         if not token:
             # TODO: show a form where they can paste in their token?
@@ -226,7 +229,7 @@ class RegistrationConsumer(AuthConsumer):
         user = self.lookup_user_by_id(user_id)
         if not user: # Maybe the user was deleted?
             return self.show_error(request, self.r_user_not_found_message)
-        
+
         # Check user is NOT active but IS in the correct group
         if self.user_is_unconfirmed(user):
             # Confirm them
@@ -237,9 +240,9 @@ class RegistrationConsumer(AuthConsumer):
             return self.on_registration_complete(request)
         else:
             return self.show_error(request, self.c_already_confirmed_message)
-    
+
     do_c.urlregex = '^c/([^/]+)/$'
-    
+
     def create_user(self, request, data, openid=None):
         from django.contrib.auth.models import User
         user = User.objects.create(
@@ -259,7 +262,7 @@ class RegistrationConsumer(AuthConsumer):
             user.set_unusable_password()
         user.save()
         return user
-    
+
     def do_password(self, request):
         "Allow users to set a password on their account"
         if request.user.is_anonymous():
@@ -278,12 +281,12 @@ class RegistrationConsumer(AuthConsumer):
             'form': form,
             'action': request.path,
         })
-    
+
     def show_password_has_been_set(self, request):
         return self.show_message(
             request, _('Password set'), _('Your password has been set.')
         )
-    
+
     def initial_from_sreg(self, sreg):
         "Maps sreg to data for populating registration form"
         fullname = sreg.get('fullname', '')
@@ -299,7 +302,7 @@ class RegistrationConsumer(AuthConsumer):
             'last_name': last_name,
             'email': sreg.get('email', ''),
         }
-    
+
     def suggest_nickname(self, nickname):
         "Return a suggested nickname that has not yet been taken"
         from django.contrib.auth.models import User
@@ -314,13 +317,13 @@ class RegistrationConsumer(AuthConsumer):
                 suffix += 1
             nickname = original_nickname + str(suffix)
         return nickname
-    
+
     def show_unknown_openid(self, request, openid):
-        # If the user gets here, they have attempted to log in using an 
-        # OpenID BUT it's an OpenID we have never seen before - so show 
+        # If the user gets here, they have attempted to log in using an
+        # OpenID BUT it's an OpenID we have never seen before - so show
         # them the index page but with an additional message
         return self.do_index(request, self.unknown_openid_message)
-    
+
     def show_already_signed_in(self, request):
         return self.show_message(
             request, _('Already signed in'), self.already_signed_in_message
